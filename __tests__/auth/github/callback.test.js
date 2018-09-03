99b597c529d8c59006bdc8c4044faef5fcb2981d
Alex Bykov <leksster@gmail.com>
2018-09-04 02:01:17 +0300

    Mock router for tests

--- __tests__/auth/github/callback.test.js
+++ __tests__/auth/github/callback.test.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Callback from '../../../pages/auth/github/callback';
+import Router from 'next/router';
 
 describe('Github Callback Page', () => {
+
   it('matches snapshot', () => {
-    const component = renderer.create(<Callback />);
+    const mockedRouter = { push: () => {} }
+    Router.router = mockedRouter;
+    const component = renderer.create(<Callback accessToken="sag" />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
